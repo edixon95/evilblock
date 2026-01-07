091a4be5262228b82a674d156718bb391ab90929
Elijah Dixon <elijah@ej-software.co.uk>
2026-01-07 17:39:25 +0000

    door structure change, scene store, scene lookup, door interact update

--- evilblock/src/characters/player/actions/handleUseDoor.jsx
+++ evilblock/src/characters/player/actions/handleUseDoor.jsx
@@ -1,3 +1,4 @@
+import { useCutsceneStore } from "../../../stores/useCutsceneStore";
 import { useGameStore } from "../../../stores/useGameStore";
 
 export const handleUseDoor = (door) => {
@@ -6,14 +7,20 @@ export const handleUseDoor = (door) => {
     const doorInformation = door.extra;
     const { cutscene, lock } = doorInformation;
 
+
+    if (cutscene) {
+        const shouldPlayCutscene = useCutsceneStore.getState().shouldCutscenePlay(cutscene)
+        if (shouldPlayCutscene) {
+            console.log("Cutscene is meant to be played", shouldPlayCutscene)
+            return;
+        } else {
+            console.log("No cutscene active for this door")
+        }
+    }
+
     if (lock.isLocked) {
         console.log("Door Locked")
         return
-    }
-
-    if (cutscene.id && cutscene.active) {
-        console.log("Cutscene to play")
-        return;
     }
 
     useGameStore.getState().handleAddData(door)
--- evilblock/src/stores/useCutsceneStore.jsx
+++ evilblock/src/stores/useCutsceneStore.jsx
@@ -1,5 +1,17 @@
 import { create } from "zustand"
+import { CUTSCENE_TABLE } from "../data/cutscenes/cutsceneTable"
 
 export const useCutsceneStore = create((set, get) => ({
-    cutscenes: []
+    cutscenes: CUTSCENE_TABLE,
+
+    shouldCutscenePlay: (id) => {
+        const { cutscenes } = get();
+        const cutscene = cutscenes[id];
+        if (!cutscene) return false
+
+        // Attach the id so it can be passed to data store
+        cutscene.id = id
+
+        return cutscene.active ? cutscene : false
+    }
 }))
